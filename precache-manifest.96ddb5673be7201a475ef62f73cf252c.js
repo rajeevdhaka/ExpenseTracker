f666b7bb1707a5e56002dbe3ec6d147ed4816c9e
Rajeev Dhaka <rajeevdhaka2001@gmail.com>
2022-07-17 02:03:41 +0530

    Updates

---
+++ precache-manifest.96ddb5673be7201a475ef62f73cf252c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f6c485584dfb54c56aafe5ff36b79f",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "ef0443b55f3858c79444",
+    "url": "/ExpenseTracker/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/ExpenseTracker/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ExpenseTracker/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef0443b55f3858c79444",
+    "url": "/ExpenseTracker/static/js/main.1af436c9.chunk.js"
+  },
+  {
+    "revision": "e25630aad13bae8f9aae",
+    "url": "/ExpenseTracker/static/js/runtime-main.0e810c6f.js"
+  }
+]);
